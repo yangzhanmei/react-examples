9835f0ea736f3c861c52e8ba4b63c8fa5a948cef
fat-gril <15809244435@163.com>
2016-08-04 11:12:45 +0800

    state and props demo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-// TODO
+const Cart = React.createClass({
+    getInitialState(){
+        return {value: 'Hello'};
+    },
+    add: function () {
+        const value = $('input[type=text]').val();
+        this.setState({value});
+    },
+    render(){
+        return (<div>
+                <div>
+                    <input type="text"/>
+                </div>
+                <button onClick={this.add}>show</button>
+                <p>{this.state.value}</p>
+                <Children name={this.state.value}/>
+            </div>
+        )
+    }
+});
+
+const Children = React.createClass({
+    render(){
+        return (<div>
+            <input type="text" value={this.props.name}/>
+        </div>)
+    }
+});
+
+ReactDOM.render(<Cart/>, document.getElementById('content'));
